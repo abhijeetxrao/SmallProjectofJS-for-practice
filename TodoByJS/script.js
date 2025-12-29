16afb96ede0dc8184e8feae1dd876ec8f9452b4e
Abhijeet Rao <abhijeetrao7800@gmail.com>
2025-12-29 20:32:21 +0530

    local storage set up

--- TodoByJS/script.js
+++ TodoByJS/script.js
@@ -1,38 +1,52 @@
-const button = document.getElementById("add-todo-btn");
-const input = document.getElementById("todo-input");
-const list = document.querySelector(".list");
+// const button = document.getElementById("add-todo-btn");
+// const input = document.getElementById("todo-input");
+// const list = document.querySelector(".list");
 
-let todoList = [];
+// let todoList = JSON.parse(localStorage.getItem("todoList")) || []
 
-function addTodo() {
-  let todoVal = input.value.trim();
-  if(!todoVal) return
-  todoList.push(todoVal);
-  console.log(todoList);
-  addList();
-  input.value = "";
-}
+// function saveToStorage(){
+//   localStorage.setItem("todoList",JSON.stringify(todoList))
+// }
 
-function addList() {
-  list.innerHTML =""
-  todoList.forEach((todo, index) => {
-    const li = document.createElement("li");
-    li.innerText = todo;
-    const delBtn = document.createElement("button");
-    delBtn.innerText = "X";
-    delBtn.addEventListener("click", () => {
-      todoList.splice(index, 1);
-      addList();
-    });
-    li.appendChild(delBtn);
-    list.appendChild(li);
-  });
-}
+// function addTodo() {
+//   let todoVal = input.value.trim();
+//   if(!todoVal) return
+//   todoList.push(todoVal);
+//   console.log(todoList);
+//   saveToStorage()
+//   addList();
+//   input.value = "";
+// }
 
-input.addEventListener("keydown",(e)=>{
-  if(e.key ==="Enter"){
-    addTodo()
-  }
-})
+// function addList() {
+//   list.innerHTML =""
+//   todoList.forEach((todo, index) => {
+//     const li = document.createElement("li");
+//     li.innerText = todo;
+//     const delBtn = document.createElement("button");
+//     delBtn.innerText = "X";
+//     delBtn.addEventListener("click", () => {
+//       todoList.splice(index, 1);
+//       saveToStorage()
+//       addList();
+//     });
+//     li.appendChild(delBtn);
+//     list.appendChild(li);
+//   });
+// }
 
-button.addEventListener("click", addTodo);
+// const localTodo = JSON.parse(localStorage.getItem("todoList"))
+
+
+// input.addEventListener("keydown",(e)=>{
+//   if(e.key ==="Enter"){
+//     addTodo()
+//   }
+// })
+
+// button.addEventListener("click", addTodo);
+
+// addList()
+
+
+const button = document.getElementById('add-todo-btn')
